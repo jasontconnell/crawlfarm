f3492c05d1997fab99bea68583e378305f3d7c59
Jason Connell <jconnell@delphicdigital.com>
2016-10-05 00:26:36 -0400

    Go fmt

--- common/crawl.go
+++ common/crawl.go
@@ -1,86 +1,86 @@
 package crawlfarmCommon
 
 import (
-    "sync"
-    "regexp"
-    "net/http"
-    "strings"
-    "io/ioutil"
-    "net/url"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+	"sync"
 )
 
 var hrefRegex *regexp.Regexp = regexp.MustCompile(`(href|src)="(.*?)"`)
 
 func Parse(site Site, referrer, content string) (urls chan Link) {
-    var wg sync.WaitGroup
-    wg.Add(1)
-    urls = make(chan Link)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	urls = make(chan Link)
 
-    go func(){
-        defer close(urls)
-        defer wg.Done()
+	go func() {
+		defer close(urls)
+		defer wg.Done()
 
-        matches := hrefRegex.FindAllStringSubmatch(content, -1)
-        for _,m := range matches {
-            href := strings.Trim(m[2], " ")
-            add := (href != "" && strings.HasPrefix(href, "/") && !strings.HasPrefix(href, "//")) || strings.HasPrefix(href, site.Root)
-            if add && !strings.HasPrefix(href, site.Root) {
-                href = site.Root + href
-            }
+		matches := hrefRegex.FindAllStringSubmatch(content, -1)
+		for _, m := range matches {
+			href := strings.Trim(m[2], " ")
+			add := (href != "" && strings.HasPrefix(href, "/") && !strings.HasPrefix(href, "//")) || strings.HasPrefix(href, site.Root)
+			if add && !strings.HasPrefix(href, site.Root) {
+				href = site.Root + href
+			}
 
-            if add {
-                urls <- Link{ Referrer: referrer, Url: href }
-            }
-        }
-    }()
+			if add {
+				urls <- Link{Referrer: referrer, Url: href}
+			}
+		}
+	}()
 
-    return
+	return
 }
 
-func Fetch(site Site, link Link) (reschan chan Result){
-    var wg sync.WaitGroup
-    wg.Add(1)
-    reschan = make(chan Result)
-    if link.Url == "" {
-        return
-    }
+func Fetch(site Site, link Link) (reschan chan Result) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	reschan = make(chan Result)
+	if link.Url == "" {
+		return
+	}
 
-    go func(){
-        defer close(reschan)
-        defer wg.Done()
+	go func() {
+		defer close(reschan)
+		defer wg.Done()
 
-        uri,parseErr := url.Parse(link.Url)
-        if parseErr != nil {
-            panic(parseErr)
-        }
+		uri, parseErr := url.Parse(link.Url)
+		if parseErr != nil {
+			panic(parseErr)
+		}
 
-        headers := make(map[string][]string)
-        if site.Headers != nil {
-            for k,v := range site.Headers {
-                headers[k] = []string{ v }
-            }
-        }
+		headers := make(map[string][]string)
+		if site.Headers != nil {
+			for k, v := range site.Headers {
+				headers[k] = []string{v}
+			}
+		}
 
-        req := &http.Request{ Method: "GET", URL: uri, Header: headers }
-        client := &http.Client{
-            CheckRedirect: func(req *http.Request, via []*http.Request) error {
-                if strings.Contains(site.Root, req.URL.Host){
-                  return nil
-                }
-                return http.ErrUseLastResponse
-            },
-        }
+		req := &http.Request{Method: "GET", URL: uri, Header: headers}
+		client := &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				if strings.Contains(site.Root, req.URL.Host) {
+					return nil
+				}
+				return http.ErrUseLastResponse
+			},
+		}
 
-        resp,err := client.Do(req)
-        
-        if err == nil {
-            defer resp.Body.Close()
-            if contents, err := ioutil.ReadAll(resp.Body); err == nil {
-                text := string(contents)
-                reschan <- Result { Link: link, Code: resp.StatusCode, Content: text }
-            }
-        }
-    }()
-    
-    return
+		resp, err := client.Do(req)
+
+		if err == nil {
+			defer resp.Body.Close()
+			if contents, err := ioutil.ReadAll(resp.Body); err == nil {
+				text := string(contents)
+				reschan <- Result{Link: link, Code: resp.StatusCode, Content: text}
+			}
+		}
+	}()
+
+	return
 }
--- server/main.go
+++ server/main.go
@@ -1,120 +1,120 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "crawlfarm/server/data"
-    "encoding/json"
-    crawl "crawlfarm/common"
-    "time"
+	crawl "crawlfarm/common"
+	"crawlfarm/server/data"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"time"
 )
 
-func init(){
-    crawl.RegisterGobs()
+func init() {
+	crawl.RegisterGobs()
 }
 
-func main(){
-    fmt.Println("Crawl Farm 1.0 - Server")
+func main() {
+	fmt.Println("Crawl Farm 1.0 - Server")
 
-    if file, err := os.OpenFile("config.json", os.O_RDONLY, os.ModePerm); err == nil {
-        var conf data.Config
-        var site crawl.Site
+	if file, err := os.OpenFile("config.json", os.O_RDONLY, os.ModePerm); err == nil {
+		var conf data.Config
+		var site crawl.Site
 
-        dec := json.NewDecoder(file)
-        dec.Decode(&conf)
-        site.Root = conf.Root
-        site.Headers = conf.Headers
+		dec := json.NewDecoder(file)
+		dec.Decode(&conf)
+		site.Root = conf.Root
+		site.Headers = conf.Headers
 
-        fmt.Println("Job:", site.Root)
+		fmt.Println("Job:", site.Root)
 
-        server := data.NewServer(site)
+		server := data.NewServer(site)
 
-        server.UnprocessedLinks <- crawl.Link { Url: conf.Root, Referrer: "/" }
+		server.UnprocessedLinks <- crawl.Link{Url: conf.Root, Referrer: "/"}
 
-        t := time.NewTicker(1 * time.Second)
-        go func(){
-            for tick := range t.C {
-                fmt.Printf("\r%v Workers: %d. Url queue: %d. Processed: %d. Errors: %d\t\t", tick.Format("15:04:05"), len(server.Workers), len(server.UnprocessedLinks), len(server.CrawledUrls), *server.ErrorCount)
-            }
-        }()
+		t := time.NewTicker(1 * time.Second)
+		go func() {
+			for tick := range t.C {
+				fmt.Printf("\r%v Workers: %d. Url queue: %d. Processed: %d. Errors: %d\t\t", tick.Format("15:04:05"), len(server.Workers), len(server.UnprocessedLinks), len(server.CrawledUrls), *server.ErrorCount)
+			}
+		}()
 
-        fmt.Println("Listen on", conf.Listen)
+		fmt.Println("Listen on", conf.Listen)
 
-        go func(){
-            if listener, err := net.Listen("tcp", conf.Listen); err == nil {
-                startListenLoop(listener, server)
-            }
-        }()
+		go func() {
+			if listener, err := net.Listen("tcp", conf.Listen); err == nil {
+				startListenLoop(listener, server)
+			}
+		}()
 
-        <-server.Finished
-        t.Stop()
+		<-server.Finished
+		t.Stop()
 
-        fmt.Println("\n\nFinished Job", site.Root)
-    }
+		fmt.Println("\n\nFinished Job", site.Root)
+	}
 }
 
-func clientConnected(conn net.Conn, server data.Server){
-    worker := data.NewWorker(conn)
-    server.Connected(worker)
+func clientConnected(conn net.Conn, server data.Server) {
+	worker := data.NewWorker(conn)
+	server.Connected(worker)
 
-    crawl.WriteSite(worker.Conn, worker.Gob, server.Site, worker.Disconnect)
-    go crawl.WriteLinks(worker.Conn, worker.Gob, worker.OutLinks, worker.Disconnect)
-    go crawl.WriteUrlCount(worker.Conn, worker.Gob, worker.FoundLinks, worker.Disconnect)
-    go crawl.ReadLoop(worker.Conn, worker.Gob, nil, server.Results, nil, worker.Disconnect)
+	crawl.WriteSite(worker.Conn, worker.Gob, server.Site, worker.Disconnect)
+	go crawl.WriteLinks(worker.Conn, worker.Gob, worker.OutLinks, worker.Disconnect)
+	go crawl.WriteUrlCount(worker.Conn, worker.Gob, worker.FoundLinks, worker.Disconnect)
+	go crawl.ReadLoop(worker.Conn, worker.Gob, nil, server.Results, nil, worker.Disconnect)
 
-    go func(){
-        <-worker.Disconnect
-        server.Disconnected(worker)
-    }()
+	go func() {
+		<-worker.Disconnect
+		server.Disconnected(worker)
+	}()
 
-    go func(){
-        <-worker.Finished
-        server.MessageLog.Println("worker finished", worker.RemoteAddr)
+	go func() {
+		<-worker.Finished
+		server.MessageLog.Println("worker finished", worker.RemoteAddr)
 
-        server.WorkerFinished(worker)
+		server.WorkerFinished(worker)
 
-        if len(server.Workers) == 0 {
-            server.Finished <- true
-        }
-    }()
+		if len(server.Workers) == 0 {
+			server.Finished <- true
+		}
+	}()
 
-    for {
-        select {
-        case link := <-server.UnprocessedLinks:
-            worker.SentLinks[link.Url] = link
-            worker.OutLinks <- link
-        case result := <-server.Results:
-            server.ProcessLog.Println(result.Link.Url)
-            server.MarkComplete(&worker, result.Link)
+	for {
+		select {
+		case link := <-server.UnprocessedLinks:
+			worker.SentLinks[link.Url] = link
+			worker.OutLinks <- link
+		case result := <-server.Results:
+			server.ProcessLog.Println(result.Link.Url)
+			server.MarkComplete(&worker, result.Link)
 
-            worker.QueueLength = result.QueueLength
-            found := 0
+			worker.QueueLength = result.QueueLength
+			found := 0
 
-            if result.Code != 200 {
-                server.RecordError(result)
-            } else {
-                for _,url := range result.Links {
-                    if server.AddUrl(url){
-                        found++
-                    }
-                }
-                worker.FoundLinks <- found
-            }
+			if result.Code != 200 {
+				server.RecordError(result)
+			} else {
+				for _, url := range result.Links {
+					if server.AddUrl(url) {
+						found++
+					}
+				}
+				worker.FoundLinks <- found
+			}
 
-            if worker.QueueLength == 0 && found == 0 && len(server.UnprocessedLinks) == 0 {
-                worker.Finished <- true
-            }
-        }
-    }
+			if worker.QueueLength == 0 && found == 0 && len(server.UnprocessedLinks) == 0 {
+				worker.Finished <- true
+			}
+		}
+	}
 
 }
 
-func startListenLoop(listener net.Listener, server data.Server){
+func startListenLoop(listener net.Listener, server data.Server) {
 
-    for {
-        if conn, err := listener.Accept(); err == nil {
-            go clientConnected(conn, server)
-        }
-    }
+	for {
+		if conn, err := listener.Accept(); err == nil {
+			go clientConnected(conn, server)
+		}
+	}
 }
